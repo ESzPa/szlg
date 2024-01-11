526e1d492e12058ab33299d7390d381241c6fd73
Ertinger <95310024+ESzPa@users.noreply.github.com>
2024-01-11 21:06:29 +0100

    4. fa

--- logokaresz/4. fa/Form1.cs
+++ logokaresz/4. fa/Form1.cs
@@ -7,13 +7,29 @@ namespace LogoKaresz
 {
 	public partial class Form1 : Form
 	{
+        void Fa(double size, int yrs, bool update)
+        {
+            if (yrs == 0) { return; }
+            using (new Frissítés(update))
+            {
+                Előre(size);
+                Balra(60);
+                Fa(size / 2, yrs - 1, update);
+                Jobbra(60);
+                Fa(size / 2, yrs - 1, update);
+                Jobbra(60);
+                Fa(size/2, yrs - 1, update);
+                Balra(60);
+                Hátra(size);
+            }
+            return;
+        }
 
 
-
-		void FELADAT()
+        void FELADAT()
 		{
-			Teleport(közép.X, közép.Y, észak);
-
+			Teleport(közép.X, közép.Y+150, észak);
+            Fa(100, 10, false);
 		}
 	}
 }
