824a2b3cba344bf47b9b5b4f9da97267ede2962f
Ertinger <95310024+ESzPa@users.noreply.github.com>
2024-01-11 14:33:13 +0100

    fa, faloop

--- logokaresz/1. fa/Form1.cs
+++ logokaresz/1. fa/Form1.cs
@@ -7,16 +7,52 @@ namespace LogoKaresz
 {
 	public partial class Form1 : Form
 	{
-		/* Függvények */
+		void Fa(double size, int yrs, bool update)
+		{
+			if (yrs == 0) { return; }
+			using(new Frissítés(update))
+			{
+				Előre(size);
+				Balra(30);
+				Fa(size / 2, yrs - 1, update);
+				Jobbra(60);
+				Fa(size/2, yrs - 1, update);
+				Balra(30);
+				Hátra(size);
+			}
+			return;
+		}
+		void FaLoop(double size, int start, int end = -1, int wait = 1000)
+		{
+            if (start < 1 || end < 1 || size < 0)
+            {
+				MessageBox.Show("Ne adj meg a FaLoopnak 1-nél kisebb számot");
+				return;
+            }
+            if (end == -1)
+			{
+				end = start;
+				start = 1;
+			}
+			for(int i = start; i <= end; i++)
+			{
+				Fa(size, i, false);
+				Pihi(wait);
+				Tollszín(Color.White);
+				Tollvastagság(10);
+				Fa(size, i, false);
+				Tollszín(Color.Black);
+				Tollvastagság(1);
+			}
+		}
 
-
-		/* Függvények vége */
 		void FELADAT()
 		{
-			/* Ezt indítja a START gomb! */
-			// Teleport(közép.X, közép.Y+150, észak);
+			Teleport(közép.X, közép.Y+170, észak);
 
-			Ív(90, 100);
+			//Fa(100, 2);
+
+			FaLoop(170, 1, 13);
 		}
 	}
 }
