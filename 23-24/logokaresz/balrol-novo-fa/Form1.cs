5db5bce2285b161fa1b1c83dc2610143f58adc04
Ertinger <95310024+ESzPa@users.noreply.github.com>
2024-01-12 08:34:24 +0100

    oop

--- 23-24/logokaresz/balrol-novo-fa/Form1.cs
+++ 23-24/logokaresz/balrol-novo-fa/Form1.cs
@@ -8,12 +8,30 @@ namespace LogoKaresz
 	public partial class Form1 : Form
 	{
 
+        void Fa(double size, int yrs, bool update)
+        {
+            if (yrs == 0) { return; }
+            using (new Frissítés(update))
+            {
+                Előre(size);
+                Balra(30);
+                Fa(size / 2, yrs - 1, update);
+                Jobbra(60);
+                Fa(size / 2, yrs - 1, update);
+                Balra(30);
+                Hátra(size/2);
+                Balra(30);
+                Fa(size/2, yrs - 2, update);
+                Jobbra(30);
+                Hátra(size / 2);
+            }
+            return;
+        }
 
-
-		void FELADAT()
+        void FELADAT()
 		{
-			Teleport(közép.X, közép.Y, észak);
-
+			Teleport(közép.X, közép.Y+150, észak);
+            Fa(10, 10, false);
 		}
 	}
 }
