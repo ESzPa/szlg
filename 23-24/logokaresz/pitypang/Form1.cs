9561f60413a3010a2f02de6d9a22dfd060866a50
Ertinger <95310024+ESzPa@users.noreply.github.com>
2024-01-12 08:14:12 +0100

    utolsonak tobbet kene visszafordulnia

--- 23-24/logokaresz/pitypang/Form1.cs
+++ 23-24/logokaresz/pitypang/Form1.cs
@@ -7,13 +7,31 @@ namespace LogoKaresz
 {
 	public partial class Form1 : Form
 	{
+        void Fa(double size, int yrs, bool update=true)
+        {
+            if (yrs == 0) { return; }
+            int fok = yrs-1;
+            if (fok == 0) { fok = 1; }
+            using (new Frissítés(update))
+            {
+                Előre(size);
+                Balra(60);
+                for(int i = 0; i < yrs; i++)
+                {
+                    Fa(size / 2, yrs - 1, update);
+                    Jobbra(120/fok);
+                }
+                Balra(60);
+                Hátra(size);
+            }
+            return;
+        }
 
 
-
-		void FELADAT()
+        void FELADAT()
 		{
-			Teleport(közép.X, közép.Y, észak);
-
+			Teleport(közép.X, közép.Y+150, észak);
+            Fa(100, 3);
 		}
 	}
 }
