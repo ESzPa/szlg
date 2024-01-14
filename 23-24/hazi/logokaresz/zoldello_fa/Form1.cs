27ff1e2e1c1d1dabb6b343d19414c378b728763e
Ertinger <95310024+ESzPa@users.noreply.github.com>
2024-01-15 00:05:22 +0100

    zoldello fa done

--- 23-24/hazi/logokaresz/zoldello_fa/Form1.cs
+++ 23-24/hazi/logokaresz/zoldello_fa/Form1.cs
@@ -7,13 +7,30 @@ namespace LogoKaresz
 {
 	public partial class Form1 : Form
 	{
-
+		void Fa(double size, int years) {
+			Color c;
+			if (years <= 0) { return; }
+			else if (years == 1) { c = Color.Green; }
+			else { c =Color.Brown; }
+			using(new Szín(c)) {
+				using(new Átmenetileg(Előre, size)) {
+					Balra(60);
+					Fa(size/1.5, years-1);
+					Jobbra(120);
+					Fa(size/1.5, years-1);
+					Balra(60);
+				}
+			}
+		}
 
 
 		void FELADAT()
 		{
-			Teleport(közép.X, közép.Y, észak);
-
+			Teleport(közép.X, közép.Y+180, észak);
+			Tollvastagság(2f);
+			using(new Frissítés(false)) {
+				Fa(150, 10);
+			}
 		}
 	}
 }
