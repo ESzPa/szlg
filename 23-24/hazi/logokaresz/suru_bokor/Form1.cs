9735a223d7cd481aed7c1b9f703f63996c454e50
Ertinger <95310024+ESzPa@users.noreply.github.com>
2024-01-14 16:47:50 +0100

    suru bokor done

--- 23-24/hazi/logokaresz/suru_bokor/Form1.cs
+++ 23-24/hazi/logokaresz/suru_bokor/Form1.cs
@@ -7,13 +7,31 @@ namespace LogoKaresz
 {
 	public partial class Form1 : Form
 	{
-
+		void Fa(double size, int years)
+		{
+			if (years <= 0) { return; }
+			using(new Átmenetileg(Előre, size)){
+				Fa(size / 2, years - 1);
+				using(new Átmenetileg(Jobbra, 30)) {
+					Fa(size/2, years - 1);
+				}
+			}
+			using(new Átmenetileg(Előre, size / 2)) {
+                Fa(size / 2, years - 1);
+                using (new Átmenetileg(Jobbra, -30)) {
+					Fa(size/2, years - 1);
+				}
+            }
+			Fa(size/2, years-1);
+        }
 
 
 		void FELADAT()
 		{
-			Teleport(közép.X, közép.Y, észak);
-
+			Teleport(közép.X, közép.Y+170, észak);
+			using(new Frissítés(false)) {
+				Fa(150, 10);
+			}
 		}
 	}
 }
