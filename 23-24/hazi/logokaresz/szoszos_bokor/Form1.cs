c075c6d92fe3f08d9e55cbb853f074e506a531a3
Ertinger <95310024+ESzPa@users.noreply.github.com>
2024-01-14 23:49:11 +0100

    szoszos bokor done

--- 23-24/hazi/logokaresz/szoszos_bokor/Form1.cs
+++ 23-24/hazi/logokaresz/szoszos_bokor/Form1.cs
@@ -7,13 +7,30 @@ namespace LogoKaresz
 {
 	public partial class Form1 : Form
 	{
-
+		void Fa(double size, int years) {
+			if (years <= 0) { return; }
+			using (new Átmenetileg(Előre, size)) {
+				Fa(size/3, years-1);
+				using(new Átmenetileg(Balra, 60)) {
+					Fa(size / 3, years - 1);
+				}
+			}
+			using(new Átmenetileg(Előre, size / 2)) {
+				Fa(size/3, years - 1);
+				using(new Átmenetileg(Jobbra, 60)) {
+					Fa(size / 3, years - 1);
+				}
+			}
+			Fa(size / 3, years - 1);
+		}
 
 
 		void FELADAT()
 		{
-			Teleport(közép.X, közép.Y, észak);
-
+			Teleport(közép.X, közép.Y+180, észak);
+			using(new Frissítés(false)) {
+				Fa(150, 8);
+			}
 		}
 	}
 }
