78427c11d7be5138ddb3dbd155f66c362946a55f
Ertinger <95310024+ESzPa@users.noreply.github.com>
2024-01-14 21:07:16 +0100

    szep fa done

--- 23-24/hazi/logokaresz/szep_fa/Form1.cs
+++ 23-24/hazi/logokaresz/szep_fa/Form1.cs
@@ -7,13 +7,30 @@ namespace LogoKaresz
 {
 	public partial class Form1 : Form
 	{
-
-
-
+		void Fa(double size, int years) {
+			Color c;
+			if (years <= 0) { return; }
+			else if (years == 1) { c = Color.LightGreen; } 
+			else if (years == 2) { c = Color.DarkGreen; }
+			else { c = Color.Brown; }
+			using(new Szín(c)) {
+				using (new Vastagság((years + 1) / 2)) {
+					using (new Átmenetileg(Előre, size)) {
+						Balra(30);
+						Fa(size / 1.5, years - 1);
+						Jobbra(30);
+						Fa(size / 1.2, years - 1);
+						Jobbra(30);
+						Fa(size / 1.5, years - 1);
+						Balra(30);
+					}
+				}
+			}
+		}
 		void FELADAT()
 		{
-			Teleport(közép.X, közép.Y, észak);
-
+			Teleport(közép.X, közép.Y+170, észak);
+			Fa(100, 5);
 		}
 	}
 }
