e6acc2bf370344add36388571a0f96c8709dd0ca
Ertinger <95310024+ESzPa@users.noreply.github.com>
2024-01-14 13:48:31 +0100

    lemaradt agu done

--- 23-24/logokaresz/lemaradt_agu_fa/Form1.cs
+++ 23-24/logokaresz/lemaradt_agu_fa/Form1.cs
@@ -7,13 +7,24 @@ namespace LogoKaresz
 {
 	public partial class Form1 : Form
 	{
-
+		void Fa(double size, int years)
+		{
+			if (years <= 0) { return; }
+			using(new Átmenetileg(Előre, size))
+			{
+				Balra(60);
+				Fa(size / 1.5, years - 2);
+				Jobbra(120);
+				Fa(size / 1.5, years-1);
+				Balra(60);
+			}
+		}
 
 
 		void FELADAT()
 		{
-			Teleport(közép.X, közép.Y, észak);
-
+			Teleport(közép.X, közép.Y+170, észak);
+			Fa(100, 10);
 		}
 	}
 }
