73d8fe25c9d190e0568af0b90826c8e1591d407c
Ertinger <95310024+ESzPa@users.noreply.github.com>
2024-01-14 20:20:26 +0100

    fakulo fa done

--- 23-24/hazi/logokaresz/fakulo_fa/Form1.cs
+++ 23-24/hazi/logokaresz/fakulo_fa/Form1.cs
@@ -7,13 +7,24 @@ namespace LogoKaresz
 {
 	public partial class Form1 : Form
 	{
-
+		void Fa(double size, int years, double trans=255) {
+			if(years <= 0) { return; }
+			using(new Szín(Color.FromArgb((byte)trans, 84, 39, 26))) {
+				using(new Átmenetileg(Előre, size)) {
+					Balra(60);
+					Fa(size / 1.5, years - 1, trans * 0.7);
+					Jobbra(120);
+					Fa(size / 1.5, years - 1, trans * 0.7);
+					Balra(60);
+				}
+			}
+		}
 
 
 		void FELADAT()
 		{
-			Teleport(közép.X, közép.Y, észak);
-
+			Teleport(közép.X, közép.Y+170, észak);
+			Fa(100, 10);
 		}
 	}
 }
