16337ad4c5cf518a5f7bee709b827f492341ea52
Ertinger <95310024+ESzPa@users.noreply.github.com>
2024-01-14 14:03:43 +0100

    suru_bokor

---
+++ 23-24/logokaresz/suru_bokor/Form1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace LogoKaresz
+{
+	public partial class Form1 : Form
+	{
+
+
+
+		void FELADAT()
+		{
+			Teleport(közép.X, közép.Y, észak);
+
+		}
+	}
+}
