1114f3e48f527e9ac0f0915ebad4be8f17d9e9e4
Ertinger <95310024+ESzPa@users.noreply.github.com>
2024-01-15 09:16:05 +0100

    weirdo done

--- 23-24/hazi/logokaresz/weirdo/Form1.cs
+++ 23-24/hazi/logokaresz/weirdo/Form1.cs
@@ -16,13 +16,29 @@ namespace LogoKaresz
 			}
 			Jobbra(30 * sign);
 		}
-	
+		void Vonalnt(double size, int num, int sign = 1) {
+            if (num <= 0) { return; }
+            Jobbra(-30 * sign);
+			Előre(size);
+            Jobbra(30 * sign);
+            Vonalnt(size, num - 1, sign * -1);
+        }
+        void Weirdo(double size, int cracks) {
+			double inner = Math.Pow(3, cracks-1);
+			if(cracks == 1) { inner = 1; }
+			for(int i= 0; i < 3; i++) {
+				Console.WriteLine(inner);
+				Vonalnt(size/inner, (int)inner);
+				Jobbra(120);
+			}
+		}
 
 
 		void FELADAT()
 		{
 			Teleport(közép.X, közép.Y+180, észak);
-			Vonal(50, 3);
-		}
+			//Vonal(50, 3);
+			Weirdo(300, 3);
+        }
 	}
 }
