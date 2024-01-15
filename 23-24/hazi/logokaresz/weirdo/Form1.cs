c45794286f9a2dee5ecf97981a07fe4376c99c98
Ertinger <95310024+ESzPa@users.noreply.github.com>
2024-01-15 10:18:50 +0100

    weirdo done

--- 23-24/hazi/logokaresz/weirdo/Form1.cs
+++ 23-24/hazi/logokaresz/weirdo/Form1.cs
@@ -6,29 +6,17 @@ using System.Windows.Forms;
 namespace LogoKaresz
 {
 	public partial class Form1 : Form {
-		void Vonal(double size, int num, int sign = 1) {
-			if (num <= 0) { return; }
-			Jobbra(-30 * sign);
-			using (new Átmenetileg(Előre, size)) {
-				Jobbra(30 * sign);
-				Vonal(size, num - 1, sign * -1);
-				Jobbra(-30 * sign);
-			}
-			Jobbra(30 * sign);
-		}
-		void Vonalnt(double size, int num, int sign = 1) {
-            if (num <= 0) { return; }
-            Jobbra(-30 * sign);
-			Előre(size);
-            Jobbra(30 * sign);
-            Vonalnt(size, num - 1, sign * -1);
+		void Vonalnt(double size, int num) {
+            if (num <= 1) { Előre(size); return; }
+			Vonalnt(size/3, num - 1);
+			Jobbra(-60);
+			Vonalnt(size/3, num-1);
+			Jobbra(60);
+			Vonalnt(size/3, num-1);
         }
         void Weirdo(double size, int cracks) {
-			double inner = Math.Pow(3, cracks-1);
-			if(cracks == 1) { inner = 1; }
 			for(int i= 0; i < 3; i++) {
-				Console.WriteLine(inner);
-				Vonalnt(size/inner, (int)inner);
+				Vonalnt(size/cracks, cracks);
 				Jobbra(120);
 			}
 		}
@@ -37,8 +25,7 @@ namespace LogoKaresz
 		void FELADAT()
 		{
 			Teleport(közép.X, közép.Y+180, észak);
-			//Vonal(50, 3);
-			Weirdo(300, 3);
+			Weirdo(500, 3);
         }
 	}
 }
