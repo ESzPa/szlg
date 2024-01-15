9176858cc6fc507d2ab16ed91b9fc9df379d747f
Ertinger <95310024+ESzPa@users.noreply.github.com>
2024-01-15 11:19:34 +0100

    koch fraktal done

--- 23-24/hazi/logokaresz/koch_fraktal/Form1.cs
+++ 23-24/hazi/logokaresz/koch_fraktal/Form1.cs
@@ -7,13 +7,25 @@ namespace LogoKaresz
 {
 	public partial class Form1 : Form
 	{
-
+		void Fraktal(double ä, int ß) {
+			if (ß <= 1) { Előre(ä); return; }
+			Fraktal(ä, ß - 1);
+			Balra(60);
+			Fraktal(ä, ß - 1);
+			Jobbra(120);
+			Fraktal(ä, ß - 1);
+			Balra(60);
+			Fraktal(ä, ß - 1);
+		}
 
 
 		void FELADAT()
 		{
-			Teleport(közép.X, közép.Y, észak);
-
+			Teleport(közép.X - 300, közép.Y+180, észak);
+			Jobbra(90);
+			using(new Frissítés(false)) {
+				Fraktal(0.2, 8);
+			}
 		}
 	}
 }
