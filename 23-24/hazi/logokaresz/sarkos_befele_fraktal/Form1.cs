83266dba54052d5ac7e9e7029babc90c24c00cc6
Ertinger <95310024+ESzPa@users.noreply.github.com>
2024-01-15 23:06:54 +0100

    sarkos befele done

--- 23-24/hazi/logokaresz/sarkos_befele_fraktal/Form1.cs
+++ 23-24/hazi/logokaresz/sarkos_befele_fraktal/Form1.cs
@@ -7,13 +7,26 @@ namespace LogoKaresz
 {
 	public partial class Form1 : Form
 	{
-
+		void Fraktal(double size, int side) {
+			if(side < 3) { return; }
+			double fordulas;
+			for(int i = 0; i < side; i++) {
+				fordulas = (180 * (side - 2) / side / 2) - (180 * (side - 3) / (side - 1) / 2); 
+                Előre(size);
+				Jobbra(180 - (180 * (side - 2) / side));
+				using(new Átmenetileg(Jobbra, fordulas)) {
+					Fraktal(size / 2, side - 1);
+				}
+			}
+		}
 
 
 		void FELADAT()
 		{
-			Teleport(közép.X, közép.Y, észak);
-
+			Teleport(közép.X-250, közép.Y+100, észak);
+			using(new Frissítés(false)) {
+				Fraktal(150, 7);
+			}
 		}
 	}
 }
