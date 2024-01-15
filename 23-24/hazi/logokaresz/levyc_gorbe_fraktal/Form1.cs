93dd04a8a9ee66c5f1f06730c02df15791a16c8c
Ertinger <95310024+ESzPa@users.noreply.github.com>
2024-01-15 14:31:44 +0100

    cgorbe done

--- 23-24/hazi/logokaresz/levyc_gorbe_fraktal/Form1.cs
+++ 23-24/hazi/logokaresz/levyc_gorbe_fraktal/Form1.cs
@@ -7,13 +7,26 @@ namespace LogoKaresz
 {
 	public partial class Form1 : Form
 	{
-
+		void Fraktal(double size, int rec) {
+			if (rec <= 1) { Előre(size); return; }
+			Jobbra(90);
+			Fraktal(size, rec-1);
+			Balra(90);
+            Fraktal(size, rec - 1);
+            Fraktal(size, rec - 1);
+			Balra(90);
+            Fraktal(size, rec - 1);
+			Jobbra(90);
+        }
 
 
 		void FELADAT()
 		{
-			Teleport(közép.X, közép.Y, észak);
-
+			Teleport(közép.X-250, közép.Y+150, észak);
+			Jobbra(90);
+			using(new Frissítés(false)) {
+				Fraktal(3, 5);
+			}
 		}
 	}
 }
