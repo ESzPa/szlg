685ed151bfa8aa1e88765408992ae9c6d1ba8a5d
Ertinger <95310024+ESzPa@users.noreply.github.com>
2024-01-15 08:39:48 +0100

    Vonal

--- 23-24/hazi/logokaresz/weirdo/Form1.cs
+++ 23-24/hazi/logokaresz/weirdo/Form1.cs
@@ -5,15 +5,24 @@ using System.Windows.Forms;
 
 namespace LogoKaresz
 {
-	public partial class Form1 : Form
-	{
-
+	public partial class Form1 : Form {
+		void Vonal(double size, int num, int sign = 1) {
+			if (num <= 0) { return; }
+			Jobbra(-30 * sign);
+			using (new Átmenetileg(Előre, size)) {
+				Jobbra(30 * sign);
+				Vonal(size, num - 1, sign * -1);
+				Jobbra(-30 * sign);
+			}
+			Jobbra(30 * sign);
+		}
+	
 
 
 		void FELADAT()
 		{
-			Teleport(közép.X, közép.Y, észak);
-
+			Teleport(közép.X, közép.Y+180, észak);
+			Vonal(50, 3);
 		}
 	}
 }
