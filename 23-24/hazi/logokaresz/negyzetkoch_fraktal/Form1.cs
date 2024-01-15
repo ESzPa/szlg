df8f3e894ac28064e40c40b5074a6a810695cf26
Ertinger <95310024+ESzPa@users.noreply.github.com>
2024-01-15 13:54:24 +0100

    negyzetkoch done

--- 23-24/hazi/logokaresz/negyzetkoch_fraktal/Form1.cs
+++ 23-24/hazi/logokaresz/negyzetkoch_fraktal/Form1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Drawing;
+using System.Security.Policy;
 using System.Threading;
 using System.Windows.Forms;
 
@@ -7,13 +8,27 @@ namespace LogoKaresz
 {
 	public partial class Form1 : Form
 	{
-
+		void Fraktal(double size, int rec, int sign=1) {
+			if(rec <= 1) { Előre(size); return; }
+			Fraktal(size, rec - 1);
+			Balra(90 * sign);
+            Fraktal(size, rec - 1, -1);
+			Jobbra(90 * sign);
+            Fraktal(size, rec - 1);
+			Jobbra(90 * sign);
+            Fraktal(size, rec - 1, -1);
+			Balra(90 * sign);
+            Fraktal(size, rec - 1);
+        }
 
 
 		void FELADAT()
 		{
-			Teleport(közép.X, közép.Y, észak);
-
+			Teleport(közép.X-250, közép.Y, észak);
+			Jobbra(90);
+			using(new Frissítés(false)) {
+                Fraktal(50, 3);
+            }
 		}
 	}
 }
