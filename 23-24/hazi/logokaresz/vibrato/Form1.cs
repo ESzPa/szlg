2db002c090ad87225fb8b5ab6a62b6cd910281e1
Ertinger <95310024+ESzPa@users.noreply.github.com>
2024-01-15 13:37:23 +0100

    vibrato done

--- 23-24/hazi/logokaresz/vibrato/Form1.cs
+++ 23-24/hazi/logokaresz/vibrato/Form1.cs
@@ -7,13 +7,27 @@ namespace LogoKaresz
 {
 	public partial class Form1 : Form
 	{
-
+		void Fraktal(double size, int rec) {
+			if (rec <= 1) { Előre(size); return; }
+			Fraktal(size, rec - 1);
+			Balra(60);
+            Fraktal(size, rec - 1);
+			Jobbra(120);
+            Fraktal(size*2, rec - 1);
+			Balra(120);
+            Fraktal(size, rec - 1);
+			Jobbra(60);
+            Fraktal(size, rec - 1);
+        }
 
 
 		void FELADAT()
 		{
-			Teleport(közép.X, közép.Y, észak);
-
+			Teleport(közép.X-250, közép.Y, észak);
+			Jobbra(90);
+			using(new Frissítés(false)) {
+				Fraktal(0.002, 10);
+			}
 		}
 	}
 }
