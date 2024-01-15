79e644f2f6e77b8504aeeae11786f808b079e252
Ertinger <95310024+ESzPa@users.noreply.github.com>
2024-01-15 15:31:57 +0100

    sierpinski nyil done

--- 23-24/hazi/logokaresz/sierpinski_nyil_fraktal/Form1.cs
+++ 23-24/hazi/logokaresz/sierpinski_nyil_fraktal/Form1.cs
@@ -7,13 +7,25 @@ namespace LogoKaresz
 {
 	public partial class Form1 : Form
 	{
-
+		void Fraktal(double size, int rec) {
+			if (rec <= 1) { Előre(size); return; }
+			Balra(60);
+			Fraktal(size, rec-1);
+			Jobbra(60);
+            Fraktal(size, rec - 1);
+			Jobbra(60);
+            Fraktal(size, rec - 1);
+			Balra(60);
+        }
 
 
 		void FELADAT()
 		{
-			Teleport(közép.X, közép.Y, észak);
-
+			Teleport(közép.X-250, közép.Y+180, észak);
+			Jobbra(90);
+			using(new Frissítés(false)) {
+				Fraktal(10, 3);
+			}
 		}
 	}
 }
