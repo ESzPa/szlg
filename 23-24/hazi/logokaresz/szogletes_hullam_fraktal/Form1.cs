66a738207fbff3d38a6fc4712bc19d534f077841
Ertinger <95310024+ESzPa@users.noreply.github.com>
2024-01-15 13:47:01 +0100

    szogletes hullam fraktal done

--- 23-24/hazi/logokaresz/szogletes_hullam_fraktal/Form1.cs
+++ 23-24/hazi/logokaresz/szogletes_hullam_fraktal/Form1.cs
@@ -7,13 +7,32 @@ namespace LogoKaresz
 {
 	public partial class Form1 : Form
 	{
-
+		void Fraktal(double size, int rec) {
+			if(rec <= 1) { Előre(size); return; }
+			Fraktal(size, rec - 1);
+			Balra(90);
+			Fraktal(size, rec - 1);
+			Jobbra(90);
+			Fraktal(size, rec - 1);
+			Jobbra(90);
+            Fraktal(size, rec - 1);
+            Fraktal(size, rec - 1);
+			Balra(90);
+            Fraktal(size, rec - 1);
+			Balra(90);
+            Fraktal(size, rec - 1);
+			Jobbra(90);
+            Fraktal(size, rec - 1);
+        }
 
 
 		void FELADAT()
 		{
-			Teleport(közép.X, közép.Y, észak);
-
+			Teleport(közép.X-250, közép.Y, észak);
+			Jobbra(90);
+			using(new Frissítés(false)) {
+				Fraktal(5, 4);
+			}
 		}
 	}
 }
