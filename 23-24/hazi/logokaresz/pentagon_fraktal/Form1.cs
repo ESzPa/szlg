7a646645e7c1d9f84d3e1851f6843e8a83a0798f
Ertinger <95310024+ESzPa@users.noreply.github.com>
2024-01-16 12:07:10 +0100

    pentagon not working

--- 23-24/hazi/logokaresz/pentagon_fraktal/Form1.cs
+++ 23-24/hazi/logokaresz/pentagon_fraktal/Form1.cs
@@ -7,13 +7,24 @@ namespace LogoKaresz
 {
 	public partial class Form1 : Form
 	{
-
+		void Fraktal5(double size, int rec) {
+			if (rec <= 0) { return; }
+            for (int i = 0; i < 5; i++) {
+                Előre(size);
+                Jobbra(72);
+				using(new Átmenetileg(Jobbra, -144)) {
+					Fraktal5(size, rec-1);
+				}
+            }
+        }
 
 
 		void FELADAT()
 		{
-			Teleport(közép.X, közép.Y, észak);
-
+			Teleport(közép.X-10, közép.Y-10, észak);
+			using(new Frissítés(false)) {
+				Fraktal5(10, 3);
+			}
 		}
 	}
 }
