0277024d52d7f59405daabdd8bff3e1234ca4a60
Ertinger <95310024+ESzPa@users.noreply.github.com>
2024-01-16 10:14:06 +0100

    shape()

--- 23-24/hazi/logokaresz/magassagtetel_fraktal/Form1.cs
+++ 23-24/hazi/logokaresz/magassagtetel_fraktal/Form1.cs
@@ -7,12 +7,24 @@ namespace LogoKaresz
 {
 	public partial class Form1 : Form
 	{
+		void Shape(double size) {
 
+			Előre(size / 2);
+			Jobbra(120);
+			Előre(Math.Sqrt((size / 2) * (size / 2) + size * size));
+			Jobbra(150);
+			Előre(size);
+			Jobbra(90);
+		}
+		void Fraktal(double size, int rec) {
+			if(rec <= 0) { }
+		}
 
 
 		void FELADAT()
 		{
 			Teleport(közép.X, közép.Y, észak);
+
 
 		}
 	}
