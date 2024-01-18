0c0ea97c67c1f29824fc7ba55521d0d00bd9b8e2
Ertinger <95310024+ESzPa@users.noreply.github.com>
2024-01-18 12:43:58 +0100

    palya06

--- 23-24/hazi/oopkaresz/palya-megoldas/Diak.cs
+++ 23-24/hazi/oopkaresz/palya-megoldas/Diak.cs
@@ -8,21 +8,37 @@ using System.Text;
 using System.Windows.Forms;
 using System.Threading;
 
-namespace Karesz
-{
-    public partial class Form1 : Form
-    {
-        void Palya02(Robot robot)
-        {
+namespace Karesz {
+    public partial class Form1 : Form {
 
+        void palya06(Robot robot) {
+            Jobbra(2);
+            while(!Van_e_előttem_fal()) {
+                Előre();
+            }
+            Jobbra(1);
+            while (!Van_e_előttem_fal()) {
+                Előre();
+            }
+            Jobbra(1);
+            while (true) {
+                Előre();
+                Jobbra();
+                while (!Van_e_előttem_fal()) {
+                    Előre();
+                }
+                Balra(2);
+                while (!Van_e_előttem_fal()) {
+                    Előre();
+                }
+                Jobbra();
+            }
         }
-        void DIÁK_ROBOTJAI()
-        {
+        void DIÁK_ROBOTJAI() {
             Robot karesz = Robot.Get("Karesz");
 
-            karesz.Feladat = delegate ()
-            {
-                Palya02(karesz);
+            karesz.Feladat = delegate () {
+                palya06(karesz);
             };
         }
     }
