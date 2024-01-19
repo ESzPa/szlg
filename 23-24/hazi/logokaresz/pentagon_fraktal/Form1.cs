d597e6602391ad3dd48b533636f2e07ce76dcb58
Ertinger <95310024+ESzPa@users.noreply.github.com>
2024-01-19 16:44:44 +0100

    pentagon fraktal

--- 23-24/hazi/logokaresz/pentagon_fraktal/Form1.cs
+++ 23-24/hazi/logokaresz/pentagon_fraktal/Form1.cs
@@ -7,14 +7,25 @@ namespace LogoKaresz
 {
 	public partial class Form1 : Form
 	{
-		void Fraktal5(double size, int rec) {
-			if (rec <= 0) { return; }
+		void Fraktal(double size, int rec) {
+            if (rec <= 1) { Előre(size); return; }
+            Fraktal(size / 2, rec - 1);
+			Jobbra(72);
+            Fraktal(size / 2, rec - 1);
+			Jobbra(-144);
+            Fraktal(size / 2, rec - 1);
+			using(new Átmenetileg(Jobbra, -72)) {
+				Fraktal(size/2, rec-1);
+            }
+            Fraktal(size / 2, rec - 1);
+			Jobbra(72);
+            Fraktal(size / 2, rec - 1);
+        }
+
+        void Fraktal5(double size, int rec) {
             for (int i = 0; i < 5; i++) {
-                Előre(size);
+				Fraktal(size, rec);
                 Jobbra(72);
-				using(new Átmenetileg(Jobbra, -144)) {
-					Fraktal5(size, rec-1);
-				}
             }
         }
 
@@ -23,7 +34,8 @@ namespace LogoKaresz
 		{
 			Teleport(közép.X-10, közép.Y-10, észak);
 			using(new Frissítés(false)) {
-				Fraktal5(10, 3);
+				//Fraktal(50, 5);
+				//Fraktal5(50, 5);
 			}
 		}
 	}
