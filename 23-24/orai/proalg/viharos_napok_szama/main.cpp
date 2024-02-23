22cfcb9f855cb91f3cabf75cdb2c81733c93d354
Ertinger <95310024+ESzPa@users.noreply.github.com>
2024-02-23 08:14:59 +0100

    viharos napok szama fixed

--- 23-24/orai/proalg/viharos_napok_szama/main.cpp
+++ 23-24/orai/proalg/viharos_napok_szama/main.cpp
@@ -4,17 +4,15 @@ using namespace std;
 int main()
 {
     size_t N;
-    int K=0, S;
-
-    cin >> N;
-
+    int K, S;
+    cin >> N >> K;
+    K = 0;
     for (size_t i = 0; i < N; i++){
         cin >> S;
         if (S > 100){
             K++;
         }
     }
-
     cout << K;
 
     return 0;
