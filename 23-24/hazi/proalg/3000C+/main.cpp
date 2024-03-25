a3230cb831aeeb8fede33263d2e196f14edcea91
ESzPa <peterszukk@gmail.com>
2024-03-25 09:12:35 +0100

    3000C+ done

--- 23-24/hazi/proalg/3000C+/main.cpp
+++ 23-24/hazi/proalg/3000C+/main.cpp
@@ -7,34 +7,38 @@ bool feladat1(std::vector<int> input);
 int feladat2(std::vector<int> input);
 int feladat3(std::vector<int> input);
 float feladat4(std::vector<int> input);
-int feladat5(std::vector<int> input);
+bool feladat5(std::vector<int> input);
 int feladat6(std::vector<int> input);
-int feladat7(std::vector<int> input);
-int feladat8(std::vector<int> input);
-int feladat9(std::vector<int> input);
+void feladat7(std::vector<int> input);
+bool feladat9(std::vector<int> input);
 int feladat10(std::vector<int> input);
-int feladat11(std::vector<int> input);
+void feladat11(std::vector<int> input, int m);
 
 int main(){
     std::ifstream f("input.txt");
     std::vector<int> input;
     int _;
+    int count = 0;
     while(f >> _){
         input.push_back(_);
+        count++;
     }
     f.close();
+
+    int m;
+    std::cin >> m;
 
     std::cout << "1. Feladat: " << feladat1(input) << "\n";
     std::cout << "2. Feladat: " << feladat2(input) << "\n";
     std::cout << "3. Feladat: " << feladat3(input) << "\n";
     std::cout << "4. Feladat: " << feladat4(input) << "\n";
-    // std::cout << "5. Feladat: " << feladat5(input) << "\n";
-    // std::cout << "6. Feladat: " << feladat6(input) << "\n";
-    // std::cout << "7. Feladat: " << feladat7(input) << "\n";
-    // std::cout << "8. Feladat: " << feladat8(input) << "\n";
-    // std::cout << "9. Feladat: " << feladat9(input) << "\n";
-    // std::cout << "10. Feladat: " << feladat10(input) << "\n";
-    // std::cout << "11. Feladat: " << feladat11(input) << "\n";
+    std::cout << "5. Feladat: " << feladat5(input) << "\n";
+    std::cout << "6. Feladat: " << feladat6(input) << "\n";
+    std::cout << "7. Feladat: "; feladat7(input); std::cout << "\n";
+    std::cout << "8. Feladat: " << count << "\n";
+    std::cout << "9. Feladat: " << feladat9(input) << "\n";
+    std::cout << "10. Feladat: " << feladat10(input) << "\n";
+    std::cout << "11. Feladat: "; feladat11(input, m); std::cout << "\n";
 
     return 0;
 }
@@ -73,4 +77,68 @@ float feladat4(std::vector<int> input){
         sum += e;
     }
     return (sum/input.size())*(sum/input.size());
+}
+
+bool feladat5(std::vector<int> input){
+    for(int e : input){
+        if(e <= 19){
+            return false;
+        }
+    }
+    return true;
+}
+
+int feladat6(std::vector<int> input){
+    int count = 0;
+    for(int e : input){
+        if(e % 19 == 0){
+            count++;
+        }
+    }
+    return count;
+}
+
+void feladat7(std::vector<int> input){
+    for(int e : input){
+        if(e % 15 == 0){
+            std::cout << e/2 << ", ";
+        }
+    }
+}
+
+bool feladat9(std::vector<int> input){
+    int prev = 1;
+    for(int e : input){
+        if(e >= 0 && prev < 0){
+            return true;
+        }
+        if(e < 0){
+            prev = -1;
+        }
+    }
+    return 0;
+}
+
+int feladat10(std::vector<int> input){
+    int min = 99999;
+    for(int e : input){
+        if(e < min){
+            min = e;
+        }
+    }
+    return min / 2;
+}
+
+void feladat11(std::vector<int> input, int m){
+    std::unordered_map<int, std::vector<int>> map;
+    for(int e : input){
+        map[e % m].push_back(e);
+    }
+    for (auto e : map) {
+        std::cout << e.first << ": ";
+        for (int value : e.second) {
+            std::cout << value << " ";
+        }
+        std::cout << "|| ";
+    }
 }
