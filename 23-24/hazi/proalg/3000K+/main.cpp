2bc98731e422bb5fab87d37aa44397be3a660e13
ESzPa <peterszukk@gmail.com>
2024-05-08 16:08:53 +0200

    beolvasas fix, Feladat 1-6

--- 23-24/hazi/proalg/3000K+/main.cpp
+++ 23-24/hazi/proalg/3000K+/main.cpp
@@ -8,7 +8,7 @@
 #include <unordered_map>
 
 
-class Student{
+class Sor{
     public:
         int sorszam;
         std::string csapat;
@@ -17,38 +17,65 @@ class Student{
         std::string helyszin;
 };
 
-std::vector<Student> Beolvasas(const char* filename){
-    std::vector<Student> students;
+std::vector<Sor> Beolvasas(const char* filename){
+    std::vector<Sor> sorok;
     std::ifstream file(filename);
     std::string line;
     while(getline(file, line)){
         std::stringstream sstream(line);
-        Student student;
+        Sor sor;
         std::string field;
         getline(sstream, field, '\t');
-        student.sorszam = std::stoi(field); 
-        getline(sstream, student.csapat, '\t');
+        sor.sorszam = std::stoi(field); 
+        getline(sstream, sor.csapat, '\t');
         getline(sstream, field, '\t');
-        student.helyezes = std::stoi(field); 
+        sor.helyezes = std::stoi(field); 
         getline(sstream, field, '\t');
-        student.sorszam = std::stoi(field); 
-        getline(sstream, student.helyszin, '\t');
-        students.push_back(student);
+        sor.ev = std::stoi(field); 
+        getline(sstream, sor.helyszin, '\t');
+        sorok.push_back(sor);
     }
     file.close();
-    return students;
+    return sorok;
 }
 
-void StudentPrint(Student student){
-    std::cout << student.sorszam << "\n";
-    std::cout << student.csapat << "\n";
-    std::cout << student.helyezes << "\n";
-    std::cout << student.ev << "\n";
-    std::cout << student.helyszin << "\n";
+void StudentPrint(Sor sor){
+    std::cout << sor.sorszam << "\n";
+    std::cout << sor.csapat << "\n";
+    std::cout << sor.helyezes << "\n";
+    std::cout << sor.ev << "\n";
+    std::cout << sor.helyszin << "\n";
+}
+
+std::vector<Sor> Feladat1_6(std::vector<Sor> sorok, std::string country){
+    std::vector<Sor> returnsorok;
+    for(Sor s : sorok){
+        if(s.csapat == country){
+            returnsorok.push_back(s);
+        }
+    }
+    return returnsorok;
+}
+
+void runFeladat1_6(std::vector<Sor> sorok, int num, std::string country){
+    std::vector<Sor> fsorok = Feladat1_6(sorok, country);
+    std::cout << "Feladat " << num << ":\n";
+    for(Sor s : fsorok){
+        std::cout << s.csapat << ": " << s.helyezes << " (" << s.ev << ", " << s.helyszin << ")\n";
+    }
 }
 
 int main(){
-    std::vector<Student> students = Beolvasas("input.txt");
+    std::vector<Sor> sorok = Beolvasas("input.txt");
+
+    runFeladat1_6(sorok, 1, "Magyarország");
+    runFeladat1_6(sorok, 2, "Anglia");
+    runFeladat1_6(sorok, 3, "Chile");
+    runFeladat1_6(sorok, 4, "Peru");
+    runFeladat1_6(sorok, 5, "Mongólia");
+    std::string str;
+    std::cin >> str;
+    runFeladat1_6(sorok, 6, str);
 
 
     return 0;
