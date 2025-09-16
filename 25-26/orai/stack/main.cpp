7408f5e1bcff4abec5dad583fa18c5af756f338d
ESzPa <peterszukk@gmail.com>
2025-09-16 09:54:06 +0200

    static stack

--- 25-26/orai/stack/main.cpp
+++ 25-26/orai/stack/main.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <stdexcept>
 
 template<typename T>
 class DStack{
@@ -26,6 +27,39 @@ public:
 
     size_t size(){
         return this->_stack.size();
+    }
+};
+
+template<typename T, size_t N>
+class SStack{
+private:
+    T _stack[N];
+    size_t _size;
+
+public:
+    SStack() : _size(0) {}
+
+    void push(T value){
+        if(this->_size >= N) std::runtime_error("Stack overflow");
+        this->_stack[this->_size++] = value;
+    }
+
+    T pop(){
+        if(this->_size == 0) std::runtime_error("Pop from empty stack");
+        --this->_size;
+    }
+
+    T& top(){
+        if(this->_size == 0) std::runtime_error("Empty stack");
+        return this->_stack[this->_size-1];
+    }
+
+    bool empty(){
+        return this->_stack == 0;
+    }
+
+    size_t size(){
+        return this->_size;
     }
 };
 
