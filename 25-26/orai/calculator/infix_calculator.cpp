a0a37f69693e30cc864bd2541ab8fb765af83249
ESzPa <peterszukk@gmail.com>
2025-10-02 09:11:43 +0200

    infix calc

--- 25-26/orai/calculator/infix_calculator.cpp
+++ 25-26/orai/calculator/infix_calculator.cpp
@@ -1,8 +1,19 @@
 #include "infix.h"
+#include "postfix.h"
 
+void infix_calculator(){
+    std::string input;
+    while(true){
+        std::cout << "> ";
+        std::cin >> input;
+        if(input == "exit") break;
+
+        uint64_t result = postfix_calculate(infix_postfix_parse(input));
+        std::cout << result << '\n';
+    }
+} 
 
 int main(void){
-    std::cout << infix_postfix_parse("((3+3)*(6*9)!)") << '\n';
-    
+    infix_calculator(); 
     return 0;
 }
