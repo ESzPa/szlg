778939b6453582e9999ce1324fe3dc0935a90bb1
ESzPa <peterszukk@gmail.com>
2025-12-03 15:51:39 +0100

    linked queue impl

--- 25-26/hazi/queue/linkedq.h
+++ 25-26/hazi/queue/linkedq.h
@@ -1,1 +1,140 @@
 #pragma once
+
+#include <memory>
+#include <stdexcept>
+
+template <typename T>
+class linked_queue {
+  private:
+    struct lq_node {
+        T value;
+        std::unique_ptr<lq_node> next;
+
+        template <typename U>
+        explicit lq_node(U&& value) : value(std::forward<U>(value)), next(nullptr) {
+        }
+    };
+
+    std::unique_ptr<lq_node> front_;
+    lq_node* rear_ = nullptr;
+    size_t size_ = 0;
+
+  public:
+    linked_queue() = default;
+
+    ~linked_queue() {
+        while(!empty()) {
+            pop();
+        }
+    }
+
+    linked_queue(const linked_queue&) = delete;
+    linked_queue& operator=(const linked_queue&) = delete;
+
+    linked_queue(linked_queue&& other) noexcept
+        : front_(std::move(other.front_)), rear_(other.rear_), size_(other.size_) {
+        other.rear_ = nullptr;
+        other.size_ = 0;
+    }
+
+    linked_queue& operator=(linked_queue&& other) noexcept {
+        if(this != &other) {
+            while(!empty()) {
+                pop();
+            }
+            front_ = std::move(other.front_);
+            rear_ = other.rear_;
+            size_ = other.size_;
+            other.rear_ = nullptr;
+            other.size_ = 0;
+        }
+
+        return *this;
+    }
+
+    const T& back() const;
+    T& back();
+    const T& front() const;
+    T& front();
+
+    template <typename U>
+    void push(U&& value);
+    void pop();
+
+    size_t size() const {
+        return size_;
+    }
+    bool empty() const {
+        return size_ == 0;
+    }
+};
+
+template <typename T>
+const T& linked_queue<T>::back() const {
+    if(empty()) {
+        throw std::runtime_error("mate, the queue is empty innit");
+    }
+
+    return rear_->value;
+}
+
+template <typename T>
+T& linked_queue<T>::back() {
+    if(empty()) {
+        throw std::runtime_error("mate, the queue is empty innit");
+    }
+
+    return rear_->value;
+}
+
+template <typename T>
+const T& linked_queue<T>::front() const {
+    if(empty()) {
+        throw std::runtime_error("mate, the queue is empty innit");
+    }
+
+    return front_->value;
+}
+
+template <typename T>
+T& linked_queue<T>::front() {
+    if(empty()) {
+        throw std::runtime_error("mate, the queue is empty innit");
+    }
+
+    return front_->value;
+}
+
+template <typename T>
+template <typename U>
+void linked_queue<T>::push(U&& value) {
+    auto new_node = std::make_unique<lq_node>(std::forward<U>(value));
+
+    if(empty()) {
+        front_ = std::move(new_node);
+        rear_ = front_.get();
+    }
+    else {
+        rear_->next = std::move(new_node);
+        rear_ = rear_->next.get();
+    }
+
+    ++size_;
+}
+
+template <typename T>
+void linked_queue<T>::pop() {
+    if(empty()) {
+        throw std::runtime_error("mate, the queue is empty innit");
+    }
+
+    if(front_.get() == rear_) {
+        front_.reset();
+        rear_ = nullptr;
+    }
+    else {
+        front_ = std::move(front_->next);
+    }
+
+    --size_;
+}
--- 25-26/hazi/queue/main.cpp
+++ 25-26/hazi/queue/main.cpp
@@ -2,16 +2,94 @@
 // Hi
 //
 
+#include <iostream>
+
 #include "dumbq.h"
 #include "linkedq.h"
 #include "realq.h"
 
+#define testq(var)                                                                                      \
+    do {                                                                                                \
+        std::cout << "Testing " << #var << ":\n";                                                       \
+        bool empty_ok = (var).empty() && (var).size() == 0;                                             \
+        std::cout << "\tInitially empty? " << (empty_ok ? "\033[32myes\033[0m" : "\033[31mno\033[0m")   \
+                  << " (size=" << (var).size() << ")\n";                                                \
+                                                                                                        \
+        bool push_ok = false;                                                                           \
+        try {                                                                                           \
+            (var).push(25);                                                                             \
+            push_ok = !(var).empty() && (var).size() == 1;                                              \
+            std::cout << "\tPush 25: " << (push_ok ? "\033[32mOK\033[0m" : "\033[31mFAIL\033[0m")       \
+                      << " (size=" << (var).size() << ")\n";                                            \
+        } catch(...) {                                                                                  \
+            std::cout << "\tPush: \033[31mFAIL\033[0m\n";                                               \
+        }                                                                                               \
+                                                                                                        \
+        if(push_ok) {                                                                                   \
+            try {                                                                                       \
+                bool front_ok = (var).front() == 25;                                                    \
+                bool back_ok = (var).back() == 25;                                                      \
+                std::cout << "\tFront/Back: "                                                           \
+                          << (front_ok && back_ok ? "\033[32mOK\033[0m" : "\033[31mFAIL\033[0m")        \
+                          << " (front=" << (var).front() << ", back=" << (var).back() << ")\n";         \
+            } catch(...) {                                                                              \
+                std::cout << "\tFront/Back: \033[31mFAIL\033[0m\n";                                     \
+            }                                                                                           \
+        }                                                                                               \
+                                                                                                        \
+        if(push_ok) {                                                                                   \
+            try {                                                                                       \
+                (var).push(50);                                                                         \
+                bool size_ok = (var).size() == 2;                                                       \
+                bool back_ok = (var).back() == 50;                                                      \
+                std::cout << "\tPush 50: "                                                              \
+                          << (size_ok && back_ok ? "\033[32mOK\033[0m" : "\033[31mFAIL\033[0m")         \
+                          << " (size=" << (var).size() << ", back=" << (var).back() << ")\n";           \
+            } catch(...) {                                                                              \
+                std::cout << "\tPush 50: \033[31mFAIL\033[0m\n";                                        \
+            }                                                                                           \
+        }                                                                                               \
+                                                                                                        \
+        if(push_ok && (var).size() >= 2) {                                                              \
+            try {                                                                                       \
+                auto old_front = (var).front();                                                         \
+                auto old_size = (var).size();                                                           \
+                (var).pop();                                                                            \
+                bool size_ok = (var).size() == old_size - 1;                                            \
+                bool front_ok = (var).front() == 50;                                                    \
+                std::cout << "\tPop " << old_front << ": "                                              \
+                          << (size_ok && front_ok ? "\033[32mOK\033[0m" : "\033[31mFAIL\033[0m")        \
+                          << " (size=" << (var).size() << ", front=" << (var).front() << ")\n";         \
+            } catch(...) {                                                                              \
+                std::cout << "\tPop: \033[31mFAIL\033[0m\n";                                            \
+            }                                                                                           \
+        }                                                                                               \
+                                                                                                        \
+        while(!(var).empty()) {                                                                         \
+            try {                                                                                       \
+                (var).pop();                                                                            \
+            } catch(...) {                                                                              \
+                break;                                                                                  \
+            }                                                                                           \
+        }                                                                                               \
+        bool empty_final = (var).empty() && (var).size() == 0;                                          \
+        std::cout << "\tAfter pops, empty? "                                                            \
+                  << (empty_final ? "\033[32myes\033[0m" : "\033[31mno\033[0m")                         \
+                  << " (size=" << (var).size() << ")\n\n";                                              \
+    } while(0)
+
 int main(int argc, char** argv) {
     // How dumb, anyways:
     dumb_queue<int> dumbq;
+    testq(dumbq);
 
     // A real one:
     real_queue<int, 256> realq;
+    testq(realq);
+
+    // Memory eater one:
+    linked_queue<int> linkedq;
+    testq(linkedq);
 
     return 0;
 }
