949476f4a5987e77133998d3554f83f2894b3cd8
ESzPa <peterszukk@gmail.com>
2026-01-22 08:48:39 +0100

    almost done

--- 25-26/hazi/meresi_hiba/4000B/main.cpp
+++ 25-26/hazi/meresi_hiba/4000B/main.cpp
@@ -1,8 +1,10 @@
 #include <algorithm>
 #include <fstream>
 #include <iostream>
+#include <set>
 #include <sstream>
 
+#include "megoldasok.hpp"
 #include "struct.hpp"
 
 netgraph parse() {
@@ -15,6 +17,7 @@ netgraph parse() {
         }
 
         std::string line;
+        std::getline(tanulok, line);
         while(std::getline(tanulok, line)) {
             std::vector<std::string> fields;
             std::stringstream ss(line);
@@ -25,11 +28,11 @@ netgraph parse() {
             }
 
             Matcsop matcsop = (fields[2] == "alfa" ? Matcsop::alfa : Matcsop::beta);
-            Ancsop ancsop = static_cast<Ancsop>(std::stoi(std::to_string(fields[3][0])));
-            Lang nyelv2 = (fields[3] == "német" ? Lang::nemet : Lang::nemet);
-            bool nem = fields[4] == "F";
-            int egyuttlakok = std::stoi(fields[5]);
-            int testverek = std::stoi(fields[6]);
+            Ancsop ancsop = static_cast<Ancsop>(fields[3][0] - '0');
+            Lang nyelv2 = (fields[4] == "német" ? Lang::nemet : Lang::spanyol);
+            bool nem = fields[5] == "F";
+            int egyuttlakok = std::stoi(fields[6]);
+            int testverek = std::stoi(fields[7]);
 
             graph.emplace_back(
                 pupil{fields[1], matcsop, ancsop, nyelv2, nem, egyuttlakok, testverek, {}});
@@ -54,7 +57,7 @@ netgraph parse() {
             }
 
             for(int j = 0; j < 36; ++j) { // Number of students
-                if(fields[i] == "1") {
+                if(fields[j] == "1") {
                     graph[i].buddies.push_back(j);
                 }
             }
@@ -70,6 +73,102 @@ netgraph parse() {
 int main(int argc, char** argv) {
     (void)argc;
     (void)argv;
+
+    netgraph graph = parse();
+
+    // 1.
+    std::cout << "1.\n";
+    // print_graphviz(graph);
+    std::cout << '\n';
+
+    // 2.
+    std::cout << "2.\n" << std::boolalpha << are_buddies(graph, "Vagd Alma", "Parady Csoma") << "\n\n";
+
+    // 3.
+    std::cout << "3.\n" << std::boolalpha << are_buddies(graph, "Vagd Alma", "Ordy Bálint") << "\n\n";
+
+    // 4.
+
+    // 5.
+    std::cout << "5.\n" << buddy_count(graph, "Ordy Bálint") << "\n\n";
+
+    // 6.
+    std::cout << "6.\n" << buddy_count(graph, "Mérta Nóra") << "\n\n";
+
+    // 7.
+    std::cout << "7.\n" << buddy_count_cmp(graph, [](size_t a, size_t b) { return a > b; }) << "\n\n";
+
+    // 8.
+    std::cout << "8.\n" << buddy_count_cmp(graph, [](size_t a, size_t b) { return a < b; }) << "\n\n";
+
+    // 9.
+    std::cout << "9.\n"
+              << std::boolalpha
+              << (buddy_count_with_property(
+                      graph, "Ne Pál",
+                      [](const netgraph& g, size_t idx) { return g[idx].matcsop == Matcsop::beta; }) > 0)
+              << "\n\n";
+
+    // 10.
+    std::cout << "10.\n"
+              << buddy_count_with_property(
+                     graph, "Ne Pál", [](const netgraph& g, size_t idx) { return g[idx].testverek > 1; })
+              << "\n\n";
+
+    // 11.
+    std::cout << "11.\n";
+    for(auto p : buddies_of(graph, "Lapos Elemér")) {
+        std::cout << p.name << "; ";
+    }
+    std::cout << "\n\n";
+
+    // 12.
+    {
+        size_t count = 0;
+        auto it = std::find_if(graph.begin(), graph.end(),
+                               [](const pupil& p) { return p.name == "Meg Gyõzõ"; });
+        count += it->buddies.size();
+        for(auto p : it->buddies) {
+            count += graph[p].testverek;
+        }
+        std::cout << "12.\n" << count << "\n\n";
+    }
+
+    // 13.
+    std::cout << "13.\n" << indirect_buddies(graph, "Szüre Tibor").size() << "\n\n";
+
+    // 14.
+    {
+        auto it = std::find_if(graph.begin(), graph.end(),
+                               [](const pupil& p) { return p.name == "Hot Elek"; });
+        std::set<size_t> idxs;
+        for(auto yin : it->buddies) {
+            idxs.insert(yin);
+            for(auto yang : graph[yin].buddies) {
+                idxs.insert(yang);
+            }
+        }
+        std::cout << "14.\n" << idxs.size() << "\n\n";
+    }
+
+    // 15.
+    {
+        auto km = std::find_if(graph.begin(), graph.end(),
+                               [](const pupil& p) { return p.name == "Kor Mihály"; });
+        auto cd = std::find_if(graph.begin(), graph.end(),
+                               [](const pupil& p) { return p.name == "Citad Ella"; });
+
+        std::vector<bool> visited(graph.size(), false);
+        std::vector<int> path;
+        if(indirect_path_search(graph, (int)(km - graph.begin()), (int)(cd - graph.begin()), visited,
+                                path)) {
+            std::cout << "15.\n";
+            for(auto e : path) {
+                std::cout << graph[e].name << " -> ";
+            }
+            std::cout << "\n\n";
+        }
+    }
 
     return 0;
 }
---
+++ 25-26/hazi/meresi_hiba/4000B/megoldasok.hpp
@@ -0,0 +1,134 @@
+#pragma once
+
+#include <algorithm>
+#include <iostream>
+#include <queue>
+
+#include "struct.hpp"
+
+// 1.
+inline void print_graphviz(const netgraph& graph) {
+    std::cout << "digraph BUDDIES {\n";
+
+    for(int i = 0; i < graph.size(); ++i) {
+        const auto& e = graph[i];
+        for(const auto& bud : e.buddies) {
+            std::cout << '\t' << i << " -> " << bud << ";\n";
+        }
+    }
+
+    std::cout << "}\n";
+}
+
+// 2. & 3.
+inline bool are_buddies(const netgraph& graph, const std::string& name1, const std::string& name2) {
+    auto it1 =
+        std::find_if(graph.begin(), graph.end(), [&name1](const pupil& p) { return p.name == name1; });
+
+    auto it2 =
+        std::find_if(graph.begin(), graph.end(), [&name2](const pupil& p) { return p.name == name2; });
+
+    size_t idx2 = it2 - graph.begin();
+
+    return std::binary_search(it1->buddies.begin(), it1->buddies.end(), idx2);
+}
+
+// 5. & 6.
+inline ssize_t buddy_count(const netgraph& graph, const std::string& name) {
+    auto it =
+        std::find_if(graph.begin(), graph.end(), [&name](const pupil& p) { return p.name == name; });
+
+    return (it == graph.end() ? -1 : it->buddies.size());
+}
+
+// 7. & 8.
+template <typename Func>
+inline std::string buddy_count_cmp(const netgraph& graph, Func cmp) {
+    pupil p = graph[0];
+    for(size_t i = 1; i < graph.size(); ++i) {
+        if(cmp(p.buddies.size(), graph[i].buddies.size())) {
+            p = graph[i];
+        }
+    }
+    return p.name;
+}
+
+// 9. & 10.
+template <typename Func>
+inline size_t buddy_count_with_property(const netgraph& graph, const std::string& name, Func check) {
+    auto it =
+        std::find_if(graph.begin(), graph.end(), [&name](const pupil& p) { return p.name == name; });
+
+    size_t count = 0;
+    for(const auto& idx : it->buddies) {
+        if(check(graph, idx)) {
+            ++count;
+        }
+    }
+
+    return count;
+}
+
+// 11. & 12.
+inline std::vector<pupil> buddies_of(const netgraph& graph, const std::string& name) {
+    auto it =
+        std::find_if(graph.begin(), graph.end(), [&name](const pupil& p) { return p.name == name; });
+
+    std::vector<pupil> buddies;
+    for(auto idx : it->buddies) {
+        buddies.push_back(graph[idx]);
+    }
+
+    return buddies;
+}
+
+// 13.
+inline std::vector<pupil> indirect_buddies(const netgraph& graph, const std::string& name) {
+    std::vector<pupil> res;
+    auto it =
+        std::find_if(graph.begin(), graph.end(), [&name](const pupil& p) { return p.name == name; });
+
+    std::vector<bool> visited(graph.size(), false);
+    std::queue<size_t> q;
+
+    size_t src = it - graph.begin();
+    visited[src] = true;
+    q.push(src);
+
+    while(!q.empty()) {
+        size_t curr = q.front();
+        q.pop();
+        res.push_back(graph[curr]);
+
+        for(auto x : graph[curr].buddies) {
+            if(!visited[x]) {
+                visited[x] = true;
+                q.push(x);
+            }
+        }
+    }
+
+    return res;
+}
+
+// 15.
+inline bool indirect_path_search(const netgraph& graph, int curr, int target, std::vector<bool>& visited,
+                                 std::vector<int>& path) {
+    visited[curr] = true;
+    path.push_back(curr);
+
+    if(curr == target) {
+        return true;
+    }
+
+    for(auto v : graph[curr].buddies) {
+        if(!visited[v]) {
+            if(indirect_path_search(graph, v, target, visited, path)) {
+                return true;
+            }
+        }
+    }
+
+    path.pop_back();
+    return false;
+}
